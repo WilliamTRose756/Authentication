bef7f88caa81f9d7e4d8c8a2ad6d992388ec7151
William Rose <willrose999@gmail.com>
2023-06-17 21:34:22 -0400

    Register user and password hashing

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000
 const app = express()
 
+app.use(express.json()) // parse json
+app.use(express.urlencoded({extended: true})) // allow sending of form data
+
+connectDB()
 
 app.use('/api/users', userRoutes)
 
