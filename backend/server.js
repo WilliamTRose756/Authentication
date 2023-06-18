10f8b6548e0396325c1098475c6861d6ce43dbb1
William Rose <willrose999@gmail.com>
2023-06-18 17:35:33 -0400

    Finish Authentication Backend

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,17 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/userRoutes.js'
 import { notFound, errorHandler} from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
+connectDB()
 const port = process.env.PORT || 5000
 const app = express()
 
+app.use(cookieParser())
 app.use(express.json()) // parse json
 app.use(express.urlencoded({extended: true})) // allow sending of form data
 
-connectDB()
 
 app.use('/api/users', userRoutes)
 
@@ -23,9 +25,3 @@ app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
 
-
-// - **POST /api/users** - Register a user
-// - **POST /api/users/auth** - Authenticate a user and get token
-// - **POST /api/users/logout** - Logout user and clear cookie
-// - **GET /api/users/profile** - Get user profile
-// - **PUT /api/users/profile** - Update profile
